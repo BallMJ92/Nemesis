8cf98d8f56326755d5166c9c470f26879fcb9456
BallMJ92 <33960596+BallMJ92@users.noreply.github.com>
2018-08-31 10:06:53 +0100

    Update SystemDumper.py

--- SystemDumper.py
+++ SystemDumper.py
@@ -1,9 +1,10 @@
-import win32evtlog
+import win32evtlog, win32api, os
 
 class SystemDumper:
 
     def __init__(self):
         self.host = "localhost"
+        
     def event_logs(self, host, logtype):
         bind = win32evtlog.OpenEventLog(host, logtype)
         flag = win32evtlog.EVENTLOG_BACKWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ
@@ -22,17 +23,32 @@ class SystemDumper:
                                                       "SourceName: %s" % str(event.SourceName), "EventType: %s" % str(event.EventType),
                                                       "StringInserts: %s" % str(event.StringInserts), "data: %s" % dataExtract]
 
-        return total, logDump
-        
-    def main(self):
+        return logDump
 
-        logt = "System"
+    def mbr_read(self):
+        """Reading Master Boot Record on Host PC"""
+        diskBootRec = os.open(r"\\.\PhysicalDrive0", os.O_RDONLY | os.O_BINARY)
+        data = os.read(diskBootRec, 512)
+        os.close(diskBootRec)
+
+        return data
+
+    def local_drives(self):
+        """Returns drive letters in use on machine"""
+        drives = win32api.GetLogicalDriveStrings()
+        drives = drives.split('\000')[:-1]
+
+        return drives
+    
+    def main(self):
+        """logt = "System"
         evlog = self.event_logs(self.host, logt)
-        print(len(evlog[1]))
-        print(evlog[0])       
-        
-        for key, value in n.items():
-            print("EventID: %s: %s\n" % (str(key), value))
+        print(len(evlog))
+
+        for key, value in evlog.items():
+            print("EventID: %s: %s\n" % (str(key), value))"""
+
+        print(self.local_drives())
 
 if __name__ == "__main__":
     dump = SystemDumper()
